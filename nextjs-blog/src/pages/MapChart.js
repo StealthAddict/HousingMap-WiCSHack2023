4230511ea2478237cf0d3350c3175c86cd2a6636
StealthAddict <iaruepprich@gmail.com>
2023-02-05 10:14:09 -0600

    Last Tweaks

--- nextjs-blog/src/pages/MapChart.js
+++ nextjs-blog/src/pages/MapChart.js
@@ -6,6 +6,11 @@ import { Tooltip, TooltipWrapper } from 'react-tooltip'
 import Overlay from 'react-bootstrap'
 
 // Tooltip not working, no time to troubleshoot
+/* Tooltip would include custom info based on particular county
+  Including: county name, median household income, median housing price,
+  nearby educational institutes.
+*/
+
 
 
 const topoGeoLink = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json"
@@ -18,14 +23,14 @@ export default function MapChart( props ) {
     .domain([10000, 100000])
     .range([
       "#1B2F11",
-    "#29481A",
-    "#375F22",
-    "#44772B",
-    "#518E33",
-    "#5FA53C",
-    "#6CBC45",
-    "#7FC45C",
-    "#91CD73"
+      "#29481A",
+      "#375F22",
+      "#44772B",
+      "#518E33",
+      "#5FA53C",
+      "#6CBC45",
+      "#7FC45C",
+      "#91CD73"
   ]);
 
   function onSelect(){
@@ -47,6 +52,7 @@ export default function MapChart( props ) {
                   geography={geo}
                   fill={colorScale(cur ? cur.Mean : "#EEE")}
                   stroke="#15312E"
+                  id={geo.properties.name}
                 />
               );
             })}
